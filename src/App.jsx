c6c01ad73288d2cf0e1514f483e49653a292f4cf
Learnwave <amarjeetsingh5072@gmail.com>
2024-03-30 15:52:25 +0530

    new feature added to file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Home from './Pages/Home/Home'
 import Navbar from './Components/Navbar/Navbar'
 const App = () => {
   return (
     <div> 
       <Navbar/> 
+      <Home/>
       <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, suscipit?</h1>
     </div>
   )
