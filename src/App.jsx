33770cfef50a0dfd4d59c432f772726ef54c69fa
Learnwave <146640293+Learnwave@users.noreply.github.com>
2024-03-31 10:34:13 +0530

    new files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Home from './pages/Home/Home'
 import Navbar from './Components/Navbar/Navbar'
+import { Routes ,Route } from 'react-router-dom'
+import Video from './pages/Video/Video'
+
 const App = () => {
   return (
     <div> 
       <Navbar/> 
-      <Home/>
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, suscipit?</h1>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+      </Routes>
+      
     </div>
   )
 }
